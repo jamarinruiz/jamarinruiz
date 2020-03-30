4674e25aa055a0b4dfea883a740141fcb38c84fd
jamarinruiz <62888373+jamarinruiz@users.noreply.github.com>
2020-03-30 15:08:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello world")
+def sumar(a,b):
+  return a+b
+
+num1 = int(input("Escribe un numero: "))
+num2 = int(input("Escribe otro numero: ))
+print("La suma de esos dos numeros es "+str(dumar(num1,num2))
